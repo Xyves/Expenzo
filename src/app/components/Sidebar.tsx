e8011c95c5f3d5966982358f8908d06a3f5c4c3b
Xyves <xodrixx@protonmail.com>
2025-06-17 20:47:19 +0200

    Feat: add icons to sidebar menu items

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,5 +1,13 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
+import {
+  CalendarDays,
+  ChartPie,
+  CreditCard,
+  LayoutDashboard,
+  Logs,
+  Settings,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
@@ -9,7 +17,7 @@ export default function Sidebar() {
   const { user } = useUser();
 
   return (
-    <aside className="  bg-[#1a1a1a] row-span-8  col-span-2">
+    <aside className="  bg-[#1a1a1a] row-span-8  col-span-2 flex flex-col">
       <ul className="flex flex-col px-6 my-4 gap-4">
         <li aria-label="Overview" className="flex flex-col ">
           <Image
@@ -21,30 +29,38 @@ export default function Sidebar() {
           />
           <p className="text-2xl text-center">{user?.username}</p>
         </li>
-        <li className="px-2 py-3 rounded-xl text-xl">Dashboard</li>
+        <li className="px-2 py-3 rounded-xl text-xl flex items-center">
+          <LayoutDashboard className="mr-2" />
+          <p>Dashboard</p>
+        </li>
         <Link href="/transactions">
           <li
             aria-label="Transactions"
-            className="px-2 py-3 rounded-xl text-xl bg-blue-700 "
+            className="px-2 py-3 rounded-xl text-xl bg-blue-700 flex items-center"
           >
-            Transactions
+            <Logs className="mr-2" />
+            <p>Transactions</p>
           </li>
         </Link>
         <Link href="expenses">
           <li
             aria-label="Transactions"
-            className="px-2 py-3 rounded-xl text-xl"
+            className="px-2 py-3 rounded-xl text-xl flex items-center"
           >
-            Expenses
+            <CreditCard className="mr-2" />
+            <p>Expenses</p>
           </li>
         </Link>
 
         <li
           aria-label="Charts"
-          className="px-2 py-3 rounded-xl text-xl"
+          className="px-2 py-3 rounded-xl text-xl flex justify-center flex-col"
           onClick={() => setIsDropDownHidden(!isDropDownHidden)}
         >
-          Charts
+          <div className="flex">
+            <ChartPie className="mr-2" />
+            <p>Charts</p>
+          </div>
           <ul className={`${!isDropDownHidden ? "hidden" : "inline"}`}>
             <Link href="/chart/categories">
               <li className="px-2 py-3 rounded-xl text-xl">Categories</li>
@@ -57,21 +73,26 @@ export default function Sidebar() {
             </Link>
           </ul>
         </li>
-        <Link href="/categories">
-          <li aria-label="Categories" className="px-2 py-3 rounded-xl text-xl">
-            Categories
+
+        <Link href="/calendar">
+          <li
+            aria-label="Calendar"
+            className="px-2 py-3 rounded-xl text-xl flex items-center"
+          >
+            <CalendarDays className="mr-2" />
+            <p>Calendar</p>
           </li>
         </Link>
+        <li aria-label="Settings" className="px-2 py-3 rounded-xl text-xl flex">
+          <Settings className="mr-2" />
+          <span>Settings</span>
+        </li>
       </ul>
-      <div>
-        <ul>
-          <li
-            aria-label="Settings"
-            className="px-2 py-3 rounded-xl text-xl"
-          ></li>
-          <li aria-label="theme" className="px-2 py-3 rounded-xl text-xl"></li>
-        </ul>
-      </div>
+      <ul className="mt-auto">
+        <li aria-label="theme" className="px-2 py-3 rounded-xl text-xl ">
+          Logout
+        </li>
+      </ul>
     </aside>
   );
 }
