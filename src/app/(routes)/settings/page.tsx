df63443c3c2e90c18e4f6a2c49d8cfa8fac4f325
Xyves <xodrixx@protonmail.com>
2025-06-21 21:38:37 +0200

    Feat: Create new routes

---
+++ src/app/(routes)/settings/page.tsx
@@ -0,0 +1,12 @@
+import MainLayout from "@/app/layout/MainLayout";
+import React from "react";
+
+export default function page() {
+  return (
+    <MainLayout>
+      <div className=" h-full bg-[#0b0b0b]">
+        <p>Settings</p>
+      </div>
+    </MainLayout>
+  );
+}
