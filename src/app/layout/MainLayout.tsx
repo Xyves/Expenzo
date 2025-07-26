cf054925fcef2e4d874bf19710b1bacfe55e7260
Xyves <xodrixx@protonmail.com>
2025-07-26 22:00:36 +0200

    Feat: Add modal portal wrapper to main layout

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import RootLayout from "../layout";
-import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import { ChildrenProps } from "../types";
+import TransactionForm from "../components/transaction/dashboard/TransactionForm";
+import ModalPortalWrapper from "./ModalPortalWrapper";
+import SelectCategory from "../components/transaction/dashboard/transactionForm/SelectCategory";
+import ClientLayoutWrapper from "./ClientLayoutWrapper";
 
 export default function MainLayout({ children }: ChildrenProps) {
   return (
     <RootLayout>
-      <div className=" grid grid-rows-8  grid-cols-12 h-full ">
-        <Sidebar />
-        <div className=" col-start-3 col-end-13 row-span-8 bg-[#1a1a1a]  h-full pr-8  py-14">
-          {children}
+      <ClientLayoutWrapper>
+        <div
+          className={` grid grid-rows-8  grid-cols-12 h-full transition-all duration-300 
+          `}
+        >
+          <Sidebar />
+          <div className=" col-start-3 col-end-14 row-span-8 bg-[#202f4c]  h-full  py-8">
+            {children}
+          </div>
         </div>
-        {/* </div> */}
-      </div>
+      </ClientLayoutWrapper>
+      <ModalPortalWrapper>
+        <TransactionForm />
+        <SelectCategory />
+      </ModalPortalWrapper>
     </RootLayout>
   );
 }
