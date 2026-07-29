9957d28761d8add548ee7e482e6f8888a0e941be
Xyves <xodrixx@protonmail.com>
2026-07-29 23:15:47 +0200

    Update deprecated apollo Providers file

--- src/lib/Providers.jsx
+++ src/lib/Providers.jsx
@@ -2,20 +2,28 @@
 
 import { Provider } from "react-redux";
 import { store } from "@/app/store";
+
 import {
   ApolloClient,
-  ApolloProvider,
   HttpLink,
   InMemoryCache,
 } from "@apollo/client";
+
+import { ApolloProvider } from "@apollo/client/react";
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:4100/graphql" }),
+  link: new HttpLink({
+    uri: "http://localhost:4000/graphql",
+  }),
   cache: new InMemoryCache(),
 });
+
 export default function Providers({ children }) {
   return (
-    <Provider store={store}>
-      <ApolloProvider client={client}>{children}</ApolloProvider>
-    </Provider>
+      <Provider store={store}>
+        <ApolloProvider client={client}>
+          {children}
+        </ApolloProvider>
+      </Provider>
   );
 }
