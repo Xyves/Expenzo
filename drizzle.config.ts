899cc9456e45275cf8ec344440e6669a5534073e
Xyves <xodrixx@protonmail.com>
2026-08-06 12:06:15 +0200

    Change drizzle dialect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
-  dialect: "mysql", // 'mysql' | 'sqlite' | 'turso'
+  dialect: "postgresql",
   schema: "./src/db/schema",
   // out: "./drizzle",
   dbCredentials: {
