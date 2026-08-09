375ec982a9a0e91b837fd77449838c55f7e70e14
Xyves <xodrixx@protonmail.com>
2026-08-09 16:11:35 +0200

    Add new zod registration schema

--- src/app/types/zod.ts
+++ src/app/types/zod.ts
@@ -4,8 +4,17 @@ export const loginSchema = z.object({
   username: z.string(),
   password: z.string().min(6),
 });
-export const registerSchema = z.object({
-  username: z.string(),
-  emailAddress: z.string().email(),
-  password: z.string().min(6),
-});
+export const registerSchema = z
+  .object({
+    username: z.string().min(4, "Username must contain at least 4 characters"),
+    emailAddress: z.email("Enter a valid email address"),
+    password: z
+      .string()
+      .min(6, "Password must contain at least 6 characters")
+      .max(20, "Password must contain less than 20 characters"),
+    passwordConfirm: z.string().min(1, "Please confirm your password"),
+  })
+  .refine((data) => data.password === data.passwordConfirm, {
+    message: "Passwords do not match",
+    path: ["passwordConfirm"],
+  });
